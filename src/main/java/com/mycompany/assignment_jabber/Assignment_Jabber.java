b6f72b255d390a745102f85349c127159d662dbd
ST10473886 <ST10473886@vcconnect.edu.za>
2025-04-24 18:09:18 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/assignment_jabber/Assignment_Jabber.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assignment_jabber;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Assignment_Jabber {
+
+    public static void main(String[] args) {
+        
+    }
+}
